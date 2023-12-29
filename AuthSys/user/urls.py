fc61400802f9a024a7994ee833c45f991c33217e
Nikhil Singh <nikhilsinghcr007@gmail.com>
2023-12-29 17:21:33 +0530

    Completed!

---
+++ AuthSys/user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.user_home, name='UserHome'),
+    path('login/', views.log_in, name='LogIn'),
+    path('signup/', views.sign_up, name='SignUp'),
+    path('logout/', views.log_out, name='LogOut'),
+]
--- AuthSys/user/views.py
+++ AuthSys/user/views.py
@@ -1,3 +1,54 @@
+from . forms import SingUpForm
 from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def user_home(request):
+    if request.user.is_authenticated:
+        return render(request, 'user/home.html', {'Username' : request.user})    
+    return HttpResponseRedirect('/user/login/')
+
+def sign_up(request):
+    if request.method == "POST":
+        try:
+            fm = SingUpForm(request.POST)
+            if fm.is_valid():
+                fm.save()
+                messages.success(request, "SignUp Successfuly!")
+                return HttpResponseRedirect('/user/')
+        except Exception as e:
+            print(e.__str__())
+            return render(request, 'user/signUp.html', {'form':fm})
+    else:
+        fm = SingUpForm()
+    return render(request, 'user/signUp.html', {'form':fm})
+
+def log_in(request):
+    fm = AuthenticationForm()
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            try:
+                fm = AuthenticationForm(request=request, data=request.POST)
+                if fm.is_valid():
+                    username = fm.cleaned_data['username']
+                    password = fm.cleaned_data['password']
+                    user = authenticate(username=username, password=password)
+                    if user:
+                        login(request=request, user=user)
+                        messages.success(request, "LogIn Successfuly!")
+                        return HttpResponseRedirect('/user/')
+                    messages.error(request, "Please Enter Correct Credentials")
+            except Exception as e:
+                print(e.__str__())
+                return render(request, 'user/logIn.html', {'form':fm})
+        else:
+            return render(request, 'user/logIn.html', {'form':fm})
+    else:
+        return HttpResponseRedirect('/user/')
+
+def log_out(request):
+    logout(request)
+    return HttpResponseRedirect('/user/login/')
